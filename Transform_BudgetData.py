6c721c919f25b7d678a4a6a7bbe890d629f04d82
CJuice <CJuice@users.noreply.github.com>
2020-06-09 13:21:14 -0400

    refactor: updated for new round of data

--- Transform_BudgetData.py
+++ Transform_BudgetData.py
@@ -14,26 +14,38 @@ def main():
 
     # IMPORTS
     import pandas as pd
+    import os
 
     # VARIABLES
+    _root_proj_path = os.path.dirname(__file__)
+    original_data_files_path = r"..\2020001_Update\OriginalData"
+    transformed_data_files_path = r"..\2020001_Update\TransformedData"
+    assert os.path.exists(original_data_files_path)
+    assert os.path.exists(transformed_data_files_path)
+
     # original_headers = ["Fiscal Year","Agency Code","Agency Name","Unit Code","Unit Name","Program Code",
     # "Program Name","Subprogram Code","Subprogram Name","Object Code","Object Name",
     # "Comptroller Subobject Code","Comptroller Subobject Name","Agency Subobject Code",
     # "Agency Subobject Name","Fund Type Name",
     # "FY 2019 Budget Book Actuals","FY 2020 Budget Book Working","FY 2021 Governors Allowance"]
 
+    source_data_file = fr"{original_data_files_path}/FY 2020 and FY 2021 Post Session for Open Data Portal - Exp Data.xlsx"
+    assert os.path.exists(source_data_file)
+    transformed_data_file = fr"{transformed_data_files_path}/FY2020through2021 - Budget - Data Only_TRANSFORMED.xlsx"
+
     common_headers = ["Fiscal Year", "Agency Code", "Agency Name", "Unit Code", "Unit Name", "Program Code",
                       "Program Name", "Subprogram Code", "Subprogram Name", "Object Code", "Object Name",
                       "Comptroller Subobject Code", "Comptroller Subobject Name", "Agency Subobject Code",
                       "Agency Subobject Name", "Fund Type Name"]
-    fy_19_headers = common_headers + ["FY 2019 Budget Book Actuals"]
-    fy_20_headers = common_headers + ["FY 2020 Budget Book Working"]
-    fy_21_headers = common_headers + ["FY 2021 Governors Allowance"]
-    source_data_file = r"OfficialData/FY 2019 Actuals through FY 2021 Allowance for Open Data Portal - Budget Figures - Data Only.xlsx"
-    source_data_file_transformed_xlsx = r"TransformedData/FY 2019 Actuals through FY 2021 Allowance for Open Data Portal - Budget Figures - Data Only_TRANSFORMED.xlsx"
-    fund_source_data_file_transformed_csv = r"TransformedData/FY 2019 Actuals through FY 2021 Allowance for Open Data Portal - Budget Figures - Data Only_TRANSFORMED.csv"
+
+    # header_working = "FY 2020 Working"
+    # header_legislative = "FY 2021 Legislative Appropriation"
+    # fy_20_headers = common_headers + [header_working]
+    # fy_21_headers = common_headers + [header_legislative]
+
     aggregation_field_name = "Budget"
-    fiscal_year = "Fiscal Year"
+    fiscal_year_header_str = "Fiscal Year"
+    fy_lead_string = "FY "
 
     # FUNCTIONS
 
@@ -42,31 +54,30 @@ def main():
     orig_df = pd.read_excel(source_data_file)
 
     # For each of the three FY's, perform the following actions.
-    # Create a dataframe for each fiscal year, only including budget data relevant to that year.
-    # Data provided by the SME included the text "FY " in front of the year value. Integer is desired, remove strings
-    # First FY
-    fy_19_only_df = orig_df[fy_19_headers]
-    fy_19_only_df = fy_19_only_df.rename(columns={"FY 2019 Budget Book Actuals": aggregation_field_name})
-    fy_19_only_df[fiscal_year] = fy_19_only_df[fiscal_year].apply(lambda x: int(x.strip("FY ")))
-    # fy_19_only_df["Type"] = "Budget-Actual"  # Completed in FME
-    # print(fy_19_only_df.info())
+    # Need to create a dataframe for each fiscal year, only including budget data relevant to that year.
+    # NOTE: Data provided by the SME included the text "FY " in front of the year value. Integer desired, remove strings
+    dataframes_list = []
+    fiscal_years_dict = {"FY 2020": "FY 2020 Working", "FY 2021": "FY 2021 Legislative Appropriation"}
 
-    # Second FY
-    fy_20_only_df = orig_df[fy_20_headers]
-    fy_20_only_df = fy_20_only_df.rename(columns={"FY 2020 Budget Book Working": aggregation_field_name})
-    fy_20_only_df[fiscal_year] = fy_20_only_df[fiscal_year].apply(lambda x: int(x.strip("FY ")))
-    # fy_20_only_df["Type"] = "Budget-Working"  # Completed in FME
-    # print(fy_20_only_df.info())
+    for fy_str, column_name in fiscal_years_dict.items():
+        print(f"\nProcessing {fy_str} column '{column_name}'")
+        fy_headers = common_headers + [column_name]
 
-    # Third FY
-    fy_21_only_df = orig_df[fy_21_headers]
-    fy_21_only_df = fy_21_only_df.rename(columns={"FY 2021 Governors Allowance": aggregation_field_name})
-    fy_21_only_df[fiscal_year] = fy_21_only_df[fiscal_year].apply(lambda x: int(x.strip("FY ")))
-    # fy_21_only_df["Type"] = "Budget-Allowance"  # Completed in FME
-    # print(fy_21_only_df.info())
+        # Need to isolate the columns of interest for the fy of focus. Pass a list of columns to a copy of the orig df
+        fy_df = orig_df.copy()[fy_headers]
+
+        # Need to rename the fy specific column to the common name for later concatenation
+        fy_df.rename(columns={column_name: aggregation_field_name}, inplace=True)
+
+        # Need to remove the 'FY ' from the fiscal year values
+        fy_df[fiscal_year_header_str] = fy_df[fiscal_year_header_str].apply(lambda x: int(x.replace(fy_lead_string, "")))
+
+        # Remnant Note, may be valuable on future rounds
+        # fy_df["Type"] = "Budget-[Working/Allowance]"  # Completed in FME, would be either of the terms in brackets []
+
+        dataframes_list.append(fy_df)
 
     # Aggregate the dataframes and concatenate into a single dataframe for output.
-    dataframes_list = [fy_19_only_df, fy_20_only_df, fy_21_only_df]
     new_master_df = pd.concat(dataframes_list)
 
     # print out some stats as a sanity check
@@ -74,13 +85,15 @@ def main():
     new_shape = new_master_df.shape
     orig_col_count = orig_shape[0]
     new_col_count = new_shape[0]
-    print(f"Original dataset was shape: {orig_df.shape}") # The new output will be 3 times the size of this output
-    print(f"Transformed dataset is shape: {new_master_df.shape}")
-    print(f"\nNew dataset record count is 3 times the original column count: {orig_col_count*3 == new_col_count}\n")
+    number_of_years_this_round = len(fiscal_years_dict)
+
+    print(f"\nOriginal dataset was shape: {orig_df.shape}")  # New output will be multiple times the size of this output
+    print(f"\nTransformed dataset is shape: {new_master_df.shape}")
+    print(f"\nNew dataset record count is {number_of_years_this_round} times the original column count: {orig_col_count*number_of_years_this_round == new_col_count}\n")
     new_master_df.info()
 
     # EXCEL OUTPUT
-    new_master_df.to_excel(source_data_file_transformed_xlsx, index=False)
+    new_master_df.to_excel(transformed_data_file, index=False)
 
     # CSV OUTPUT
     # new_master_df.to_csv(path_or_buf=fund_source_data_file_transformed_csv, index=False)
