c7fab3a0da2b9162637124f3a840ba4a7cc2e9e3
CJuice <CJuice@users.noreply.github.com>
2020-06-12 10:34:47 -0400

    Create doit_fundingsource_main.py

---
+++ doit_fundingsource_main.py
@@ -0,0 +1,26 @@
+"""
+Author:
+Created:
+Revisions:
+"""
+
+
+def main():
+
+    # IMPORTS
+
+    # VARIABLES
+
+    # ASSERTS
+    
+    # FUNCTIONS
+
+    # CLASSES
+
+    # FUNCTIONALITY
+
+    return
+
+
+if __name__ == "__main__":
+    main()
