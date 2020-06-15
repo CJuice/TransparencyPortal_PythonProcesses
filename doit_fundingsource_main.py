a0f2b2f7457bfe65095a128c7e907df56c844abc
CJuice <CJuice@users.noreply.github.com>
2020-06-15 10:26:04 -0400

    refactor: joining tables to create end product

--- doit_fundingsource_main.py
+++ doit_fundingsource_main.py
@@ -52,39 +52,53 @@ def main():
         pd.set_option('display.width', None)
         # pd.set_option('display.max_colwidth', -1)
 
-    data_df = pd.read_excel(io=transformed_data_file)
-    print(data_df.info())
+    # Need to control the dtypes to avoid conversion of strings like 'Program Code' to integers (stripping leading zero)
+    column_dtypes = ['Fiscal Year', 'Agency Code', 'Agency Name', 'Unit Code', 'Unit Name',
+                     'Program Code', 'Program Name', 'Fund Type Name', 'Fund Source Code',
+                     'Fund Source Name']
+    budget_dtype = {"Budget": "float"}
+    master_dtypes = {**budget_dtype, **{header: str for header in column_dtypes}}
+
+    # Need the data with appropriate dtypes as df
+    data_df = pd.read_excel(io=transformed_data_file, dtype=master_dtypes)
 
     # Excel versions - Won't work do to encoding issue
     # state_programs_df = pd.read_excel(io=state_program_descriptions_file)
     # agency_categories_df = pd.read_excel(io=agency_categories_file)
 
-    # CSV versions
+    # CSV versions - This method does not convert '05' into an integer but leaves it as string, without using dtypes
+    #   In addition, could provide encoding to bypass issue with bytes
     state_programs_df = pd.read_csv(filepath_or_buffer=state_program_descriptions_file, encoding="Windows-1252")
     agency_categories_df = pd.read_csv(filepath_or_buffer=agency_categories_file)
 
     # Need to create the Organization Code column and populate in the data dataframe
     data_df["Organization Code"] = data_df.apply(
-        lambda row: f"{row['Agency Code']}_{row['Unit Code']}_{row['Program Code']}", axis=1)
-    print(data_df)
+        lambda row: str(f"{row['Agency Code']}_{row['Unit Code']}_{row['Program Code']}"), axis=1)
 
     # Need to create the Organization Code column and populate in the State Programs Descriptions dataframe
     state_programs_df["Organization Code"] = state_programs_df.apply(
-        lambda row: f"{row['AgencyCode']}_{row['UnitCode']}_{row['ProgramCode']}", axis=1)
-    state_programs_df.rename(columns={"AgencyCode": "Agency Code"}, inplace=True)
-    print(state_programs_df)
+        lambda row: str(f"{row['AgencyCode']}_{row['UnitCode']}_{row['ProgramCode']}"), axis=1)
 
-    print(state_programs_df.info())
-    print(agency_categories_df.info())
-    # exit()
+    # Only need the 'Description' field joined to data table, drop all others
+    org_code_parts = ["AgencyCode", "UnitCode", "ProgramCode", "AgencyName", "UnitName", "ProgramName"]
+    state_programs_df.drop(columns=org_code_parts, inplace=True)
 
-    # FIXME: Joins not working
     # Need to join the state programs data to the data_df on Organization Code using left join
-    first_join_df = data_df.join(other=state_programs_df, on="Organization Code", how="left", lsuffix="data", rsuffix="spd")
+    # data_df.set_index(keys="Organization Code", inplace=True, drop=True)
+    state_programs_df.set_index(keys="Organization Code", inplace=True, drop=True)
+
+    first_join_df = data_df.join(other=state_programs_df, on="Organization Code")
     print(first_join_df)
 
     # Need to join the agency categories data to the first join df on Agency Code using left join
-    second_join_df = first_join_df.join(other=agency_categories_df, on="Agency Code", how="left", lsuffix="data", rsuffix="ac")
+    first_join_df.info()
+    agency_categories_drop = ["Agency Name"]
+    agency_categories_df.drop(columns=agency_categories_drop, inplace=True)
+    agency_categories_df.set_index(keys="Agency Code", drop=True, inplace=True)
+    second_join_df = first_join_df.join(other=agency_categories_df, on="Agency Code")
+    print(second_join_df)
+
+
     return
 
 
