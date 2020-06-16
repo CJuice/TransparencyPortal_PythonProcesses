9fde68089b3fab822f9b4f3f85d2827347fd9cf2
CJuice <CJuice@users.noreply.github.com>
2020-06-16 09:42:53 -0400

    Create doit_centralizedvariables.py

---
+++ doit_centralizedvariables.py
@@ -0,0 +1,19 @@
+"""
+TODO
+"""
+
+budget_common_headers = ["Fiscal Year", "Agency Code", "Agency Name", "Unit Code", "Unit Name", "Program Code",
+                          "Program Name", "Subprogram Code", "Subprogram Name", "Object Code", "Object Name",
+                          "Comptroller Subobject Code", "Comptroller Subobject Name", "Agency Subobject Code",
+                          "Agency Subobject Name", "Fund Type Name"]
+funding_common_headers = ['Fiscal Year', 'Agency Code', 'Agency Name', 'Unit Code', 'Unit Name',
+                          'Program Code', 'Program Name', 'Fund Type Name', 'Fund Source Code',
+                          'Fund Source Name']
+fte_common_headers = ["Agency Code", "Unit Code", "Program Code"]
+cur_cr_common_headers = ["Fiscal Year", "Agency Code", "Agency Name", "Unit Code", "Unit Name", "Program Code",
+                          "Program Name", "Subprogram Code", "Subprogram Name", "Object Code", "Object Name",
+                          "Comptroller Subobject Code", "Comptroller Subobject Name", "Agency Subobject Code",
+                          "Agency Subobject Name", "Fund Type Name"]
+
+agency_categories_file = r"../20200601_Update/20200609_ExtraRequiredFiles/20200127_AgencyCategories_CJuiceCleaned.csv"
+state_program_descriptions_file = r"../20200601_Update/20200609_ExtraRequiredFiles/20200127_StateProgramDescriptions.csv"
