99d4419fa06e167e9ab737f2a4164d26f7938c29
CJuice <CJuice@users.noreply.github.com>
2020-06-17 09:15:28 -0400

    feat: adding variablse and asserts

--- doit_concatenate_old_new_data_main.py
+++ doit_concatenate_old_new_data_main.py
@@ -1,6 +1,7 @@
 """
-Author:
-Created:
+TODO
+Author: CJuice
+Created: 20200617
 Revisions:
 """
 
@@ -8,16 +9,43 @@ Revisions:
 def main():
 
     # IMPORTS
+    import datetime
+    import doit_centralizedvariables as myvars
+    import os
+    import pandas as pd
 
     # VARIABLES
+    _root_project_path = os.path.dirname(__file__)
+    today_str = datetime.datetime.today().strftime("%Y%m%d")
+    historical_data_folder = r"..\20200601_Update\20200609_FilteredFYProductionData"
+    updates_data_folder = r"..\20200601_Update\20200615_PythonResults"
+    full_pandas_df_printing = True
+
+    # TODO: WIth some forethought on naming conventions for outputs a hard coded mapping could be abandoned.
+    historical_to_update_files_mapping = {"Maryland_Operating_Budget__CUR_and_CR_FILTERED_FYs.csv": "20200616_CUR-CR_pythonoutput.csv",
+                                       "Maryland_Operating_Budget_-_Funding_Source_FILTERED_FYs.csv": "20200615_Funding_pythonoutput.csv",
+                                       "Maryland_Operating_Budget_FILTERED_FYs.csv": "20200616_Budget_pythonoutput.csv",
+                                       "State_of_Maryland_Full_Time_Equivalents_FILTERED_FYs.csv": "20200617_FTE_pythonoutput.csv"}
+    historical_to_update_paths_mapping = {os.path.join(historical_data_folder, key): os.path.join(updates_data_folder, value) for key, value in historical_to_update_files_mapping.items()}
 
     # ASSERTS
-    
+    assert os.path.exists(historical_data_folder)
+    assert os.path.exists(updates_data_folder)
+    for key, value in historical_to_update_paths_mapping.items():
+        assert os.path.exists(key)
+        assert os.path.exists(value)
+
     # FUNCTIONS
 
     # CLASSES
 
     # FUNCTIONALITY
+    if full_pandas_df_printing:
+        # pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', None)
+        # pd.set_option('display.max_colwidth', -1)
+
 
     return
 
