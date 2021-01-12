6515ae68184a376d1d765ccd3c528d512fc4e061
CJuice <CJuice@users.noreply.github.com>
2021-01-12 14:54:31 -0500

    refactor: print statements and switch to excel use

--- doit_operatingbudget_main.py
+++ doit_operatingbudget_main.py
@@ -17,32 +17,32 @@ def main():
     import pandas as pd
 
     # VARIABLES
-    _root_project_path = os.path.dirname(__file__)
+
     today_str = datetime.datetime.today().strftime("%Y%m%d")
 
-    agency_categories_file = myvars.agency_categories_file
     budget_actual = "Budget - Actual"
     budget_appropriation = "Budget - Appropriation"
     budget_column_dtype = {"Budget": "float"}
     budget_working = "Budget - Working"
+
+    # Will need to be changed every year
     budget_type_values_dict = {2017: budget_actual, 2018: budget_actual, 2019: budget_actual,
-                               2020: budget_working, 2021: budget_appropriation}
+                               2020: budget_actual, 2021: budget_working, 2022: budget_appropriation}
     column_headers = myvars.budget_common_headers
     data_category = "Budget"
     drop_fields = ["AgencyCode", "UnitCode", "ProgramCode", "AgencyName", "UnitName", "ProgramName"]
     full_pandas_df_printing = True
     org_code_str = "Organization Code"
-    output_result_csv = fr"../20200601_Update/20200615_PythonResults/{today_str}_{data_category}_pythonoutput.csv"
-    state_program_descriptions_file = myvars.state_program_descriptions_file
-    transformed_data_file = fr"../20200601_Update/20200609_TransformedData/FY2020through2021 - {data_category} - Data Only_TRANSFORMED.xlsx"
+    output_result_csv = fr"{myvars.python_results_folder}/{today_str}_{data_category}_pythonoutput.csv"
+    transformed_data_file = fr"{myvars.transformed_data_folder}/FY{myvars.first}_{myvars.third}_{data_category}_TRANSFORMED.xlsx"
 
     # For TESTING, smaller file size for faster testing/processing
-    # transformed_data_file = fr"../20200601_Update/20200609_TransformedData/FY2020through2021 - {data_category} - Data Only_TRANSFORMED__SLIM.xlsx"
-    # print("Using SLIM data file.")
+    transformed_data_file = fr"{myvars.transformed_data_folder}/FY{myvars.first}_{myvars.third}_{data_category}_TRANSFORMED_SLIM.xlsx"
+    print(f"Using SLIM data file. {transformed_data_file}")
 
     # ASSERTS
-    assert os.path.exists(agency_categories_file)
-    assert os.path.exists(state_program_descriptions_file)
+    assert os.path.exists(myvars.agency_categories_file)
+    assert os.path.exists(myvars.state_program_descriptions_file)
     assert os.path.exists(transformed_data_file)
 
     # FUNCTIONALITY
@@ -50,25 +50,29 @@ def main():
         # pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
         pd.set_option('display.width', None)
-        # pd.set_option('display.max_colwidth', -1)
+        # pd.set_option('display.max_colwidth', None)
 
     master_dtypes = {**budget_column_dtype, **{header: str for header in column_headers}}
 
     # Need the data as df with appropriate dtypes to avoid conversion of strings like 'Program Code' to integers (stripping leading zero)
     data_df = pd.read_excel(io=transformed_data_file, dtype=master_dtypes)
-    print(data_df.info())
+    print(f"Transformed Data: \n{data_df.info()}")
 
-    # Excel versions - Won't work do to encoding issue
-    # state_programs_df = pd.read_excel(io=state_program_descriptions_file)
-    # agency_categories_df = pd.read_excel(io=agency_categories_file)
+    # Excel versions - Sometimes doesn't work do to encoding issue. Fallback to csv's in that situation.
+    state_programs_df = pd.read_excel(io=myvars.state_program_descriptions_file, dtype={"ProgramCode": str})
+    print(f"State Program Descriptions: \n{state_programs_df.info()}")
+
+    agency_categories_df = pd.read_excel(io=myvars.agency_categories_file)
+    print(f"Agency Categories: \n{agency_categories_df.info()}")
 
     # CSV versions - This method does not convert '05' into an integer but leaves it as string, without using dtypes
     #   In addition, could provide encoding to bypass issue with bytes
-    state_programs_df = pd.read_csv(filepath_or_buffer=state_program_descriptions_file, encoding="Windows-1252")
-    agency_categories_df = pd.read_csv(filepath_or_buffer=agency_categories_file)
+    # state_programs_df = pd.read_csv(filepath_or_buffer=myvars.state_program_descriptions_file, encoding="Windows-1252")
+    # agency_categories_df = pd.read_csv(filepath_or_buffer=myvars.agency_categories_file)
 
     # Need to create the Type field and map values based on FY
-    data_df["Type"] = data_df.apply(lambda row: budget_type_values_dict.get(int(row["Fiscal Year"])), axis=1)
+    data_df["Type"] = data_df.apply(
+        lambda row: budget_type_values_dict.get(int(row["Fiscal Year"])), axis=1)
 
     # Need to create the Organization Code column and populate in the data dataframe
     data_df[org_code_str] = data_df.apply(
@@ -76,7 +80,7 @@ def main():
 
     # Need to create the Organization Sub Code column and populate in the data dataframe
     data_df["Organization Sub Code"] = data_df.apply(
-        lambda row: str(f"{row['Organization Code']}_{row['Subprogram Code']}"), axis=1)
+        lambda row: str(f"{row[org_code_str]}_{row['Subprogram Code']}"), axis=1)
 
     # Need to create the Organization Code column and populate in the State Programs Descriptions dataframe
     state_programs_df[org_code_str] = state_programs_df.apply(
@@ -89,16 +93,19 @@ def main():
     # data_df.set_index(keys="Organization Code", inplace=True, drop=True)
     state_programs_df.set_index(keys=org_code_str, inplace=True, drop=True)
 
-    first_join_df = data_df.join(other=state_programs_df, on="Organization Code")
-    print(first_join_df)
+    first_join_df = data_df.join(other=state_programs_df, on=org_code_str)
+    print(f"First Join:")
+    # print(first_join_df)
+    first_join_df.info()
 
     # Need to join the agency categories data to the first join df on Agency Code using left join
-    first_join_df.info()
     agency_categories_drop = ["Agency Name"]
     agency_categories_df.drop(columns=agency_categories_drop, inplace=True)
     agency_categories_df.set_index(keys="Agency Code", drop=True, inplace=True)
     second_join_df = first_join_df.join(other=agency_categories_df, on="Agency Code")
-    print(second_join_df)
+    print(f"Second Join:")
+    # print(second_join_df)
+    second_join_df.info()
 
     second_join_df.to_csv(path_or_buf=output_result_csv, index=False)
     return
