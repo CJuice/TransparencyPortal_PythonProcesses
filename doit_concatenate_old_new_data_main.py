13804ecd788d4cc6eb133c684f684395779109b5
CJuice <CJuice@users.noreply.github.com>
2021-01-13 14:04:47 -0500

    refactor: reduced hard coding of names, use central vars, work with output names from prev process

--- doit_concatenate_old_new_data_main.py
+++ doit_concatenate_old_new_data_main.py
@@ -1,11 +1,13 @@
 """
-Concatenate the processed update data files with their matching historical data files and output to csv.
-Iterate over the pairs of update data and corresponding historical data, concatenate them into a single resource,
-and write that resource to csv for upload to open data platform as staging assets.
+Concatenate the processed update data files with their matching historical filtered data files and output to csv.
+Iterate over the pairs of update data and corresponding historical filtered data, concatenate them into a
+single resource, and write that resource to csv for upload to open data platform as staging assets.
 
 Author: CJuice
 Created: 20200617
 Revisions:
+    20210112, CJuice, Refactored variables to allow easier editing each year. Also redesigned to work with output
+        of previous step to enable step to step running without reconfiguring variables.
 """
 
 
@@ -15,31 +17,62 @@ def main():
     import datetime
     import os
     import pandas as pd
+    import doit_centralizedvariables as myvars
 
     # VARIABLES
-    _root_project_path = os.path.dirname(__file__)
     today_str = datetime.datetime.today().strftime("%Y%m%d")
-    historical_data_folder = r"..\20200601_Update\20200609_FilteredFYProductionData"
-    updates_data_folder = r"..\20200601_Update\20200615_PythonResults"
-    concatenated_data_folder = r"..\20200601_Update\20200617_ConcatenatedDataProduct"
     full_pandas_df_printing = True
 
-    # TODO: WIth some forethought on naming conventions for outputs a hard coded mapping could be abandoned.
-    historical_to_update_files_mapping = {"CUR_CR": ("Maryland_Operating_Budget__CUR_and_CR_FILTERED_FYs.csv", "20200617_CUR-CR_pythonoutput.csv"),
-                                       "Funding": ("Maryland_Operating_Budget_-_Funding_Source_FILTERED_FYs.csv", "20200615_Funding_pythonoutput.csv"),
-                                       "Budget": ("Maryland_Operating_Budget_FILTERED_FYs.csv", "20200616_Budget_pythonoutput.csv"),
-                                       "FTE": ("State_of_Maryland_Full_Time_Equivalents_FILTERED_FYs.csv", "20200617_FTE_pythonoutput.csv")}
-    historical_to_update_paths_mapping = {style: (os.path.join(historical_data_folder, values[0]), os.path.join(updates_data_folder, values[1])) for style, values in historical_to_update_files_mapping.items()}
-    print(historical_to_update_paths_mapping)
+    processed_files_list = None
+    filtered_files_list = None
 
-    # ASSERTS
-    assert os.path.exists(historical_data_folder)
-    assert os.path.exists(updates_data_folder)
-    assert os.path.exists(concatenated_data_folder)
+    for directory, sub_folders, files in os.walk(myvars.processed_data_folder):
+        processed_files_list = files
+    for directory, sub_folders, files in os.walk(myvars.filtered_data_folder):
+        filtered_files_list = files
+
+    # TODO: Refactor below to function when have time to consider
+    cur_cr_filtered, funding_filtered, budget_filtered, fte_filtered = None, None, None, None
+    cur_cr_processed, funding_processed, budget_processed, fte_processed = None, None, None, None
+    for name in processed_files_list:
+        if myvars.cur_cr_data_type in name:
+            cur_cr_processed = name
+        elif myvars.funding_data_type in name:
+            funding_processed = name
+        elif myvars.fte_data_type in name:
+            fte_processed = name
+        elif myvars.budget_data_type in name:
+            budget_processed = name
+        else:
+            print("Issue while assigning files to variables")
+            exit()
+    for name in filtered_files_list:
+        if myvars.cur_cr_data_type in name:
+            cur_cr_filtered = name
+        elif myvars.funding_data_type in name:
+            funding_filtered = name
+        elif myvars.fte_data_type in name:
+            fte_filtered = name
+        elif myvars.budget_data_type in name:
+            budget_filtered = name
+        else:
+            print("Issue while assigning files to variables")
+            exit()
+
+    historical_to_update_files_mapping = {myvars.cur_cr_data_type: (cur_cr_filtered, cur_cr_processed),
+                                          myvars.funding_data_type: (funding_filtered, funding_processed),
+                                          myvars.budget_data_type: (budget_filtered, budget_processed),
+                                          myvars.fte_data_type: (fte_filtered, fte_processed)}
+
+    historical_to_update_paths_mapping = {style: (os.path.join(myvars.filtered_data_folder, values[0]), os.path.join(myvars.processed_data_folder, values[1])) for style, values in historical_to_update_files_mapping.items()}
+    print(historical_to_update_paths_mapping)
+    assert os.path.exists(myvars.filtered_data_folder)
+    assert os.path.exists(myvars.processed_data_folder)
+    assert os.path.exists(myvars.combined_data_folder)
     for key, value in historical_to_update_paths_mapping.items():
-        historical, update = value
-        assert os.path.exists(historical)
-        assert os.path.exists(update)
+        filtered, processed = value
+        assert os.path.exists(filtered)
+        assert os.path.exists(processed)
 
     # FUNCTIONALITY
     if full_pandas_df_printing:
@@ -49,14 +82,17 @@ def main():
         # pd.set_option('display.max_colwidth', -1)
 
     for style, paths_tup in historical_to_update_paths_mapping.items():
-        historical, update = paths_tup
-        new_concatenated_file = os.path.join(concatenated_data_folder, f"{today_str}_UPDATED_{style}.csv")
-        hist_df = pd.read_csv(historical, low_memory=False)
-        update_df = pd.read_csv(update, low_memory=False)
-        print(f"\n{style}\n\tHistorical\n{hist_df.info()}")
-        print(f"\tUpdate\n{update_df.info()}")
-        new_df = pd.concat(objs=[hist_df, update_df], ignore_index=True, sort=True, copy=False)
-        new_df.info()
+        filtered, processed = paths_tup
+        new_concatenated_file = os.path.join(myvars.combined_data_folder, f"{today_str}_{style}_COMBINED.csv")
+        filtered_df = pd.read_csv(filtered, low_memory=False)
+        processed_df = pd.read_csv(processed, low_memory=False)
+        print(f"\n{style}\n\tFiltered")
+        print(filtered_df.info())
+        print(f"\n{style}\n\tProcessed")
+        print(processed_df.info())
+        new_df = pd.concat(objs=[filtered_df, processed_df], ignore_index=True, sort=True, copy=False)
+        print(f"\n{style}\n\tCombined")
+        print(new_df.info())
         new_df.to_csv(path_or_buf=new_concatenated_file, index=False)
 
     return
